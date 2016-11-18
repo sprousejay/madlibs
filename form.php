8ca9c75040215d9afb125f261bc6f7134b5a22bb
Jay Sprouse <jaysprouse2689@gmail.com>
2016-11-17 19:27:27 -0600

    changed form and paragraph up to funcions in the top php tag

--- form.php
+++ form.php
@@ -2,47 +2,80 @@
 
 	function adjective () {
 		$adjective = $_GET["adjective"];
-			echo $adjective;
+			return $adjective;
 	}
 
 	function noun () {
 		$noun = $_GET["noun"];
-			echo $noun;
+			return $noun;
 	}
 	
 	function verb () {
 		$verb = $_GET["verb"];
-			echo $verb;
+			return $verb;
 	}
 	
 	function nationality () {
 		$nationality = $_GET["nationality"];
-			echo $nationality;
+			return $nationality;
 	}
 	
 	function food () {
-		$foods = $_GET["food"];
-			echo $food;
+		$food = $_GET["food"];
+			return $food;
 	}
 
 	function number () {
 		$number = $_GET["number"];
-			echo $number;
+			return $number;
 	}
 
 	function shapes () {
 		$shapes = $_GET["shapes"];
-			echo $shapes;
+			return $shapes;
 	}
 
 	function pluralnoun () {
 		$pluralnoun = $_GET["pluralnoun"];
-			echo $pluralnoun;
+			return $pluralnoun;
 	}
 
 	function person () {
 		$person	= $_GET["person"];
-			echo $person;
+			return $person;
+	}
+
+	function paragraph () {
+		if ($_GET) {
+			echo '<p>Pizza was invented by a <span class="words">' . adjective () . '</span>, <span class="words">' . nationality () . '</span>chef named <span class="words">' . person () . '</span>. To make a pizza, you need to take a lump of <span class="words">' . noun () . '</span>, and make a thin, round <span class="words">' . adjective () . '</span> <span class="words">' . noun () . '</span>. Then you cover it with <span class="words">' . adjective () . '</span> sauce, <span class="words">' . adjective () . '</span> cheese, and fresh chopped <span class="words">' . pluralnoun () . '</span>. Next you have to bake it in a very hot <span class="words">' . noun () . '</span>. When it is done, cut it into <span class="words">' . number () . '</span> <span class="words">' . shapes () . '</span>. Some kids like <span class="words"> ' . food () . '</span> pizza the best, but my favorite is the <span class="words">' . food () . '</span> pizza. If I could, I would eat pizza <span class="words">' . number () . '</span> times a day!</p>';
+
+		} 
+
+		else {
+			echo '<form>
+				Adjective<br>
+				<input type="text" name="adjective"><br>
+				Noun<br>
+				<input type="text" name="noun"><br>
+				Verb<br>
+				<input type="text" name="verb"><br>
+				Person<br>
+				<input type="text" name="person"><br>
+				Shapes<br>
+				<input type="text" name="shapes"><br>
+				Plural Noun<br>
+				<input type="text" name="pluralnoun"><br>
+				Number<br>
+				<input type="text" name="number"><br>
+				Food<br>
+				<input type="text" name="food"><br>
+				Nationality<br>
+				<input type="text" name="nationality"><br>
+
+				<input type="submit">
+				</form>';
+			
+			}
 	}
 ?>
 
@@ -55,31 +88,10 @@
 <body>
 
 	
-	<form>
-		Adjective<br>
-		<input type="text" name="adjective"><br>
-		Noun<br>
-		<input type="text" name="noun"><br>
-		Verb<br>
-		<input type="text" name="verb"><br>
-		Person<br>
-		<input type="text" name="person"><br>
-		Shapes<br>
-		<input type="text" name="shapes"><br>
-		Plural Noun<br>
-		<input type="text" name="pluralnoun"><br>
-		Number<br>
-		<input type="text" name="number"><br>
-		Food<br>
-		<input type="text" name="food"><br>
-		Nationality<br>
-		<input type="text" name="nationality"><br>
+	
 
-		<input type="submit">
-	</form>
-
-
-	<p>Pizza was invented by a <span class="words"><?php adjective (); ?></span>, <span class="words"><?php nationality (); ?></span>chef named <span class="words"><?php person (); ?></span>. To make a pizza, you need to take a lump of <span class="words"><?php noun (); ?></span>, and make a thin, round <span class="words"><?php adjective (); ?></span> <span class="words"><?php noun (); ?></span>. Then you cover it with <span class="words"><?php adjective (); ?></span> sauce, <span class="words"><?php adjective (); ?></span> cheese, and fresh chopped <span class="words"><?php pluralnoun (); ?></span>. Next you have to bake it in a very hot <span class="words"><?php noun (); ?></span>. When it is done, cut it into <span class="words"><?php number (); ?></span> <span class="words"><?php shapes (); ?></span>. Some kids like <span class="words"><?php food (); ?></span> pizza the best, but my favorite is the <span class="words"><?php food (); ?></span> pizza. If I could, I would eat pizza <span class="words"><?php number (); ?></span> times a day!</p>
+<?php paragraph (); ?>
+	
 
 
 </body>
